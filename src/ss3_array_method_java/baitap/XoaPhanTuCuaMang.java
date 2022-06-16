d8d8df56780fc4b2913a2270b9f62be497c6214f
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2022-06-16 19:49:43 +0700

    up

--- src/ss3_array_method_java/baitap/XoaPhanTuCuaMang.java
+++ src/ss3_array_method_java/baitap/XoaPhanTuCuaMang.java
@@ -1,8 +1,35 @@
 package ss3_array_method_java.baitap;
+
 import java.util.Scanner;
+
 public class XoaPhanTuCuaMang {
     public static void main(String[] args) {
+        int n, i, c;
         Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Nhập vào số phần tử của mảng: ");
+            n = scanner.nextInt();
+        } while (n <= 0);
 
+        int arr[] = new int[n];
+
+        for (i = 0; i < n; i++) {
+            System.out.print("Nhập phần tử thứ " + i + ": ");
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.print("Nhập số cần xoá: ");
+        int k = scanner.nextInt();
+        for (c = i = 0; i < n; i++) {
+            if (arr[i] != k) {
+                arr[c] = arr[i];
+                c++;
+            }
+        }
+        n = c;
+        System.out.print("Mảng còn lại sau khi xóa phần tử " + k + " là: ");
+        for (i = 0; i < n; i++) {
+            System.out.print(arr[i] + "\t");
+        }
     }
 }
