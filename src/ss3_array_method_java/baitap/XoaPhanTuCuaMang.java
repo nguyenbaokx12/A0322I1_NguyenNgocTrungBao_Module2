0de5364a999204164dc89f27c632f41a80e8ba48
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2022-06-18 11:53:32 +0700

    nop bai

--- src/ss3_array_method_java/baitap/XoaPhanTuCuaMang.java
+++ src/ss3_array_method_java/baitap/XoaPhanTuCuaMang.java
@@ -1,35 +1,40 @@
 package ss3_array_method_java.baitap;
 
+
 import java.util.Scanner;
 
 public class XoaPhanTuCuaMang {
     public static void main(String[] args) {
-        int n, i, c;
+        int n, i;
         Scanner scanner = new Scanner(System.in);
-        do {
-            System.out.println("Nhập vào số phần tử của mảng: ");
+        do{
+            System.out.print("Nhập số phần tử của mảng: ");
             n = scanner.nextInt();
-        } while (n <= 0);
+        }while(n<=0);
 
-        int arr[] = new int[n];
+        int arr[]= new int[n];
 
-        for (i = 0; i < n; i++) {
-            System.out.print("Nhập phần tử thứ " + i + ": ");
+        for(i = 0; i < n; i++){
+            System.out.print("Nhập phần tử thứ "+i+": ");
             arr[i] = scanner.nextInt();
         }
 
-        System.out.print("Nhập số cần xoá: ");
+        System.out.print("Nhập phần tử cần xoá: ");
         int k = scanner.nextInt();
-        for (c = i = 0; i < n; i++) {
-            if (arr[i] != k) {
-                arr[c] = arr[i];
-                c++;
+        int viTri = 0;
+        for(i = 0; i <= arr.length; i++){
+            if(k == arr[i]){
+                viTri = i;
+                break;
             }
         }
-        n = c;
-        System.out.print("Mảng còn lại sau khi xóa phần tử " + k + " là: ");
-        for (i = 0; i < n; i++) {
-            System.out.print(arr[i] + "\t");
+        for (i = viTri; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+
+        System.out.print("Mảng sau khi xoá: ");
+        for (i = 0; i < arr.length - 1; i++) {
+            System.out.print(arr[i]+" ");
         }
     }
 }
